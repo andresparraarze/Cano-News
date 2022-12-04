28dba8ea87624bb402e28a2dd901f57c7a714af3
Parra <andresparraarze@gmail.com>
2022-12-03 19:57:11 -0500

    update of initial documents

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <div>
+            <div>
+                <p className='m-5 font-bold'>
+                    Cano News
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
